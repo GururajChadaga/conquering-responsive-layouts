5b2c8f7aeb45848c28f175104f1eceac3894da2c
gururajchadaga <guruuraj.chadaga@gmail.com>
2021-08-26 21:47:03 +0530

    Hero and sales points

--- final-challenge/main.js
+++ final-challenge/main.js
@@ -1,7 +1,10 @@
 const navToggle = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.nav');
-
+const header = document.querySelector('header');
+const hero = document.querySelector('.hero');
 
 navToggle.addEventListener('click', () => {
-    nav.classList.toggle('nav--visible');
-})
+  nav.classList.toggle('nav--visible');
+  header.classList.toggle('header--expand');
+  hero.classList.toggle('hero--shrink');
+});
